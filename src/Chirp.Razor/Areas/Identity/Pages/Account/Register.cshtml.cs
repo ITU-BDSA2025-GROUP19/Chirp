b63d6da5d508c34b4ae14b066f9f41c418467adb
Hans Larsen <havl@itu.dk>
2025-11-01 11:43:00 +0100

    Add user name to register

--- src/Chirp.Razor/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Chirp.Razor/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -70,6 +70,9 @@ namespace Chirp.Razor.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [Display(Name = "Username")]
+            public string DisplayName { get; set; } = string.Empty;
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
