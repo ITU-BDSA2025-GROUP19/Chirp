be8e88f5ee64904d6e3cd088563b194b0b01884e
SkootyPex <kallej@outlook.dk>
2025-11-02 18:21:55 +0100

    add sending of cheeps for authenticated users - Input box -author dupe, now checking exiting author via email isntead of named - implement onpostasync on public and usertimeline

--- src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
@@ -3,21 +3,28 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Chirp.Application.Interfaces;
 using Chirp.Application.DTOs;
+using Chirp.Infrastructure.Repositories;
 
 namespace Chirp.Razor.Pages;
 
 public class UserTimelineModel : PageModel
 {
     private readonly ICheepService _service;
+
+    private readonly CheepRepository _cheepRepository;
     private readonly UserManager<IdentityUser> _userManager;
 
     public List<CheepDto> Cheeps { get; set; } = new();
     public int CurrentPage { get; set; } = 1;
     public string Author { get; set; } = "";
 
-    public UserTimelineModel(ICheepService service, UserManager<IdentityUser> userManager)
+    [BindProperty]
+    public string Text { get; set; } = string.Empty;
+
+    public UserTimelineModel(ICheepService service, UserManager<IdentityUser> userManager, CheepRepository cheeprepository)
     {
         _service = service;
+        _cheepRepository = cheeprepository;
         _userManager = userManager;
     }
 
@@ -45,6 +52,24 @@ public class UserTimelineModel : PageModel
             Cheeps = await _service.GetCheepsByAuthor(Author, page);
         }
         return Page();
+
+    }
     
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!User.Identity.IsAuthenticated)
+        {
+            return Challenge();
+        }
+
+        var user = await _userManager.GetUserAsync(User);
+
+        if (user == null) {
+            return Forbid();
+        }
+
+        await _cheepRepository.CreateCheepAsync(user.UserName ?? "Unknown", user.Email ?? "", Text);
+
+        return RedirectToPage();
     }
 }
