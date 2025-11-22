6eda68073549830740e44d3cfa5e8289b0321ddc
Hans Larsen <havl@itu.dk>
2025-11-22 16:59:19 +0100

    Remove email confirmation from registration flow
    Email confirmation steps have been removed from the user registration process. New users are now signed in immediately after registration without requiring email verification.

--- src/Chirp.Razor/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Chirp.Razor/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -142,27 +142,9 @@ namespace Chirp.Razor.Areas.Identity.Pages.Account
                     _context.Authors.Add(author);
                     await _context.SaveChangesAsync();
 
-                    var userId = await _userManager.GetUserIdAsync(user);
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
+                    // Skip email confirmation entirely:
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
                 }
                 foreach (var error in result.Errors)
                 {
